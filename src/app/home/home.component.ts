f5cfb50929a7e47decb028bbb15e08549713cc0f
Wayne Starr <racer159@live.com>
2022-01-03 00:00:15 -0600

    wip: added scroll button functionality + cleanup

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  r159HomeExperience: HTMLElement | null = null;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.r159HomeExperience = document.getElementById('r159-home-experience');
+  }
+
+  scrollExperience(direction: string) {
+    let scroll = this.r159HomeExperience?.scrollLeft || 0;
+
+    console.log(this.r159HomeExperience);
+
+    if (direction === 'right') {
+      scroll += 396;
+    } else if (direction === 'left') {
+      scroll -= 396;
+    }
+
+    this.r159HomeExperience?.scroll({top:0, left: scroll, behavior:'smooth'});
   }
 
 }
