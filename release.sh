3f5641c041018d63c7311e9236fe9b42a7b87805
Wayne Starr <racer159@live.com>
2022-01-30 17:12:47 -0600

    Build from 7ba412b376ecddbe576ee83fd5a685bddb43c6a9

--- release.sh
+++ release.sh
@@ -5,6 +5,6 @@ ng build
 # Add everything in dist/racer159-com
 (cd dist/racer159-com; git add --all)
 # ommit, with a message to link to sources commit
-(cd dist/racer159-com; git commit -m "Build output as of $(git log '--format=format:%H' master -1))"
+(cd dist/racer159-com; git commit -m "Build from $(git log '--format=format:%H' master -1)")
 # 5. Push to origin
 git push origin gh-pages
