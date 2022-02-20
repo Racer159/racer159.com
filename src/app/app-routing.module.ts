c2f932f10241e2e5933a6dae9d7da8151b900c4e
Wayne Starr <racer159@live.com>
2022-02-19 22:18:21 -0600

    feat: add privacy policy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DeveloperComponent } from './developer/developer.component';
+import { PrivacyComponent } from './privacy/privacy.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'developer', component: DeveloperComponent }
+  { path: 'developer', component: DeveloperComponent },
+  { path: 'privacy', component: PrivacyComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DeveloperComponent } from './developer/developer.component';
+import { PrivacyComponent } from './privacy/privacy.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    DeveloperComponent
+    DeveloperComponent,
+    PrivacyComponent
   ],
   imports: [
     BrowserModule,
