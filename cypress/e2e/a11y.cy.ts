362b0a35f5fdc6a8042feffbe08aee953e474ab5
Wayne Starr <me@racer159.com>
2022-08-14 20:13:39 -0500

    Update node dependencies for website

--- cypress/e2e/a11y.cy.ts
+++ cypress/e2e/a11y.cy.ts
@@ -1,24 +1,28 @@
 describe('Accessibility Test', () => {
   it('feat: home meets base wcag requirements', () => {
     cy.visit('/');
+    cy.wait(250); // Wait for animations to finish
     cy.injectAxe();
     cy.checkA11y();
   });
 
   it('feat: developer mode meets base wcag requirements', () => {
     cy.visit('/developer');
+    cy.wait(250); // Wait for animations to finish
     cy.injectAxe();
     cy.checkA11y();
   });
 
   it('feat: privacy policy meets base wcag requirements', () => {
     cy.visit('/privacy');
+    cy.wait(250); // Wait for animations to finish
     cy.injectAxe();
     cy.checkA11y();
   });
 
   it('feat: info page meets base wcag requirements', () => {
     cy.visit('/info');
+    cy.wait(250); // Wait for animations to finish
     cy.injectAxe();
     cy.checkA11y();
   });
