31eec43037a434079ec78ab260ed112dd85ea65d
Wayne Starr <me@racer159.com>
2024-12-31 18:03:20 -0700

    update tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,13 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterModule
       ],
       declarations: [
         AppComponent
--- src/app/developer/developer.component.spec.ts
+++ src/app/developer/developer.component.spec.ts
@@ -1,6 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { RouterModule } from '@angular/router';
 
 import { DeveloperComponent } from './developer.component';
 
@@ -10,7 +10,7 @@ describe('DeveloperComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ RouterTestingModule ],
+      imports: [ RouterModule ],
       declarations: [ DeveloperComponent ],
       schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
     })
